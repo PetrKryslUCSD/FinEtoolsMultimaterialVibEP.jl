83814d083bfb0eadbfbc72795d00b249daf73761
Petr Krysl <pkrysl@ucsd.edu>
2020-04-04 16:10:57 -0700

    Write out Matlab output

--- src/FinEtoolsMultimaterialVibEP.jl
+++ src/FinEtoolsMultimaterialVibEP.jl
@@ -12,6 +12,7 @@ using FinEtools.MeshExportModule
 using LinearAlgebra
 using SparseArrays
 using Arpack
+using MAT
 
 function solve_ep(parameterfile)
 
@@ -64,28 +65,28 @@ function solve_ep(parameterfile)
     d,v,nev,nconv = eigs(K+OmegaShift*M, M; nev=neigvs, which=:SM)
     d = d .- OmegaShift;
     fs = real(sqrt.(complex(d)))/(2*pi)
-    println("Eigenvalues: $fs [Hz]")
-    open(meshfilebase * "-eval" * ".mat", "w") do file
-        writedlm(file, d)
-        # write(file, matrix[:])
-    end
+    # println("Eigenvalues: $fs [Hz]")
+    # open(meshfilebase * "-eval" * ".mat", "w") do file
+    #     writedlm(file, d)
+    #     # write(file, matrix[:])
+    # end
     # mode = 7
     # scattersysvec!(u, v[:,mode])
     # File =  "multimaterial_nas.vtk"
     # vtkexportmesh(File, fens, allfes; vectors=[("mode$mode", u.values)])
     # @async run(`"paraview.exe" $File`)
-    for i in 1:length(d)
-        open(meshfilebase * "-evec$(i)" * ".mat", "w") do file
-            writedlm(file, v[:, i])
-            # write(file, matrix[:])
-        end
-    end
+    # for i in 1:length(d)
+    #     open(meshfilebase * "-evec$(i)" * ".mat", "w") do file
+    #         writedlm(file, v[:, i])
+    #         # write(file, matrix[:])
+    #     end
+    # end
 
     # Extract the boundary
     bfes = meshboundary(allfes)
     bconn = connasarray(bfes)
 
-    MESH.write_MESH("$(meshfilebase)-surface.mesh", fens, bfes) 
+    # MESH.write_MESH("$(meshfilebase)-surface.mesh", fens, bfes) 
 
     # Create a machine for the surface integrals, and calculate the coupling structural-acoustic matrix. Note that the acoustic fluid properties are not used in this method and they are supplied as dummy values.
     bfemm = FEMMAcoustSurf(IntegDomain(bfes, SimplexRule(2, 1)), MatAcoustFluid(1.0, 1.0))
@@ -98,19 +99,31 @@ function solve_ep(parameterfile)
         areas[panel] = integratefunction(femm1, geom, (x) ->  1.0)
     end
 
-    open(meshfilebase * "-areas" * ".mat", "w") do file
-        writedlm(file, areas)
-        # write(file, matrix[:])
-    end
+    # open(meshfilebase * "-areas" * ".mat", "w") do file
+    #     writedlm(file, areas)
+    #     # write(file, matrix[:])
+    # end
 
     I, J, V =  SparseArrays.findnz(G)
-    open(meshfilebase * "-G" * ".mat", "w") do file
-        for i in 1:length(I)
-            @printf(file, "%d, %d, %g\n", I[i], J[i], V[i])
-        end
-        # writedlm(file, hcat(I, J, V))
-        # write(file, matrix[:])
-    end
+    # open(meshfilebase * "-G" * ".mat", "w") do file
+    #     for i in 1:length(I)
+    #         @printf(file, "%d, %d, %g\n", I[i], J[i], V[i])
+    #     end
+    #     # writedlm(file, hcat(I, J, V))
+    #     # write(file, matrix[:])
+    # end
+
+    Omega = d
+    E = v
+    X = fens.xyz
+    conn = bconn
+    file = matopen(meshfilebase * ".mat", "w")
+    write(file, "Omega", d)
+    write(file, "E", v)
+    write(file, "X", fens.xyz)
+    write(file, "conn", bconn)
+    write(file, "G", G)
+    close(file)
 
     true
 
