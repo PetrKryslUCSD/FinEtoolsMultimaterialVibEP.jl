79b7ad57f7bdf979ef230659ef77aad9380c10a4
Petr Krysl <pkrysl@ucsd.edu>
2022-11-12 11:51:04 -0800

    update tests

--- test/test_two_blocks.jl
+++ test/test_two_blocks.jl
@@ -3,7 +3,14 @@ using Test
 using FinEtoolsMultimaterialVibEP: solve_ep
 
 # The name of the parameter file is up to you
-parameterfile = "twoblocks.json"
+parameterfile = joinpath(@__DIR__, "twoblocks.json")
+solve_ep(parameterfile)
+
+@test isfile("twoblocks.mat")
+
+
+# The name of the parameter file is up to you
+parameterfile = joinpath(@__DIR__, "twoblocks_ssit.json")
 solve_ep(parameterfile)
 
 @test isfile("twoblocks.mat")
